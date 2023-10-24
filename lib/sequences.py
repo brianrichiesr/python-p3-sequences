662c0d1fbb096930cdc5f141c0c771f78b95a248
Brian Richie Sr <brianrichiesr@gmail.com>
2023-10-23 23:51:28 -0700

    complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if not length:
+        print([])
+    elif length == 1:
+        print([0])
+    else:
+        fib_list = [0, 1]
+        for key in range(2, length):
+            num = fib_list[key - 1] + fib_list[key - 2]
+            fib_list.append(num)
+        print(fib_list)
